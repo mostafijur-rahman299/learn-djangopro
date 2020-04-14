68043db86113b05ae99e5351384458760d9880af
mostafijru-rahman299 <mostafijur-rahman299@gmail.com>
2020-04-14 19:55:24 +0600

    custom and builtin mixin

--- apps/urls.py
+++ apps/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import learn_template_view
+from .views import learn_template_view, LearnListView
 
 urlpatterns = [
-	path('learn-template/', learn_template_view, name='learn-template')
+	path('learn-template/', learn_template_view, name='learn-template'),
+	path('learn-list/', LearnListView.as_view(), name='learn-list')
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import LearnTemplate
 from apps.decorator import is_usersuper
@@ -20,3 +22,21 @@ def learn_template_view(request):
 	return render(request, "apps/learn_templates.html", context)
 
 
+#custom mixin
+class LearnMixin(object):
+	def get_context_data(self, *args, **kwargs):
+		context = super().get_context_data(*args, **kwargs)
+		context['mix_title'] = "Hello Mixin"
+		return context 
+
+class LearnMixin2(object):
+	def form_valid(self, form):
+		pass 
+
+	def form_invalid(self, form):
+		pass
+		
+
+class LearnListView(LoginRequiredMixin, LearnMixin, ListView):
+	model = LearnTemplate
+	template_name = "apps/form.html"
