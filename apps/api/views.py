c25d6cfc4dc53e3898a4aaa72486c86edf7cfc29
mostafijru-rahman299 <mostafijur-rahman299@gmail.com>
2020-04-29 14:47:37 +0600

    drf pagination

--- apps/api/views.py
+++ apps/api/views.py
@@ -7,6 +7,13 @@ from rest_framework import permissions
 from django.db.models import Q
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import pagination
+
+
+class StandartResultpagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 
 class LearnTemplateViewset(viewsets.ViewSet):
@@ -26,11 +33,13 @@ class LearnTemplateViewset(viewsets.ViewSet):
 class LearnTemplateModelViewSet(viewsets.ModelViewSet):
 	queryset = LearnTemplate.objects.filter(Q(name__icontains='s')|Q(name__icontains='m'))
 	serializer_class = LearnTemplateSerializer
+	pagination_class = StandartResultpagination
 
 
 class LearnTemplateAPIViewFiltering(generics.ListAPIView):
 	serializer_class = LearnTemplateSerializer
 	queryset = LearnTemplate.objects.all()
+	pagination_class = StandartResultpagination
 	filter_backends = [DjangoFilterBackend]
 	filterset_fields = ['name', 'created_at']
 	search_fields = ['name', 'description']
